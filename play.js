98473344406911b0986f089150f7df86df18b88c
bkcatton <ben.k.catton@gmail.com>
2022-01-22 18:47:56 -0800

    Refactored exports and imports using es6 shorthand.

--- play.js
+++ play.js
@@ -1,21 +1,8 @@
 const net = require("net");
+const {connect} = require('./client');
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.0.73',
-    port: 50541
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log(data.toString());
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
